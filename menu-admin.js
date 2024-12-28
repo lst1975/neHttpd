17c5543f8273230dc4e54f5b80d4e962939feb53
lst1975 <980680431@qq.com>
2024-12-28 23:48:34 +0800

    Delete menu-admin.js

--- menu-admin.js
+++
@@ -1,5 +0,0 @@
-var ___leftIcon=[
-  {name: "DeviceInfo", lang:gmtLangBuild(["DeviceInfo"],1), ln:"config/system.png", isSys: 0},
-  {name: "Config", lang:gmtLangBuild(["Config"],1), ln:"config/config.png", isSys: 0},
-  {name: "SystemUpgrade", lang:gmtLangBuild(["SystemUpgrade"],1), ln:"config/upgrade.png", isSys: 0},
-];
