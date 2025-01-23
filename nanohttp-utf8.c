b72d2ff529557c196c1e9356e1ea30e2677ea60a
lst1975 <980680431@qq.com>
2025-01-23 23:01:01 +0800

    Add files via upload

--- nanohttp-utf8.c
+++ nanohttp-utf8.c
@@ -94,83 +94,6 @@
  * 0xE0-0xEF: 3
  * 0xF0-0xF4: 4
  */
-/* Return 0 - success,  >0 - index(1 based) of first error char */
-int ng_is_utf8_str(const unsigned char *data, int len)
-{
-  int err_pos = 1;
-
-  while (len) 
-  {
-    int bytes;
-    const unsigned char byte1 = data[0];
-
-    /* 00..7F */
-    if (byte1 <= 0x7F) 
-    {
-      bytes = 1;
-      /* C2..DF, 80..BF */
-    } 
-    else if (len >= 2 && byte1 >= 0xC2 && byte1 <= 0xDF &&
-        (signed char)data[1] <= (signed char)0xBF) 
-    {
-      bytes = 2;
-    } 
-    else if (len >= 3) {
-      const unsigned char byte2 = data[1];
-
-      /* Is byte2, byte3 between 0x80 ~ 0xBF */
-      const int byte2_ok = (signed char)byte2 <= (signed char)0xBF;
-      const int byte3_ok = (signed char)data[2] <= (signed char)0xBF;
-
-      if (byte2_ok && byte3_ok &&
-         /* E0, A0..BF, 80..BF */
-        ((byte1 == 0xE0 && byte2 >= 0xA0) ||
-         /* E1..EC, 80..BF, 80..BF */
-         (byte1 >= 0xE1 && byte1 <= 0xEC) ||
-         /* ED, 80..9F, 80..BF */
-         (byte1 == 0xED && byte2 <= 0x9F) ||
-         /* EE..EF, 80..BF, 80..BF */
-         (byte1 >= 0xEE && byte1 <= 0xEF))) 
-      {
-        bytes = 3;
-      } 
-      else if (len >= 4) 
-      {
-        /* Is byte4 between 0x80 ~ 0xBF */
-        const int byte4_ok = (signed char)data[3] <= (signed char)0xBF;
-
-        if (byte2_ok && byte3_ok && byte4_ok &&
-           /* F0, 90..BF, 80..BF, 80..BF */
-          ((byte1 == 0xF0 && byte2 >= 0x90) ||
-           /* F1..F3, 80..BF, 80..BF, 80..BF */
-           (byte1 >= 0xF1 && byte1 <= 0xF3) ||
-           /* F4, 80..8F, 80..BF, 80..BF */
-           (byte1 == 0xF4 && byte2 <= 0x8F))) 
-        {
-          bytes = 4;
-        } 
-        else 
-        {
-          return err_pos;
-        }
-      }
-      else 
-      {
-        return err_pos;
-      }
-    }
-    else 
-    {
-      return err_pos;
-    }
-
-    len -= bytes;
-    err_pos += bytes;
-    data += bytes;
-  }
-
-  return 0;
-}
 
 /* Return 0 - success,  >0 - index(1 based) of first error char */
 int ng_utf8_charlen(const unsigned char *data, int len)
@@ -242,4 +165,24 @@ int ng_utf8_charlen(const unsigned char *data, int len)
   return err_pos;
 }
 
+/* Return 0 - success,  >0 - index(1 based) of first error char */
+int ng_is_utf8_str(const unsigned char *data, int len)
+{
+  int err_pos = 1;
+
+  while (len) 
+  {
+    int bytes = ng_utf8_charlen(data, len);
+    if (bytes == 1)
+    {
+      return err_pos;
+    }
+    
+    len -= bytes;
+    err_pos += bytes;
+    data += bytes;
+  }
+
+  return 0;
+}
 
